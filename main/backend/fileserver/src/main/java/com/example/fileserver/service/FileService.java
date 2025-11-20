d44b8935c81ddfe78e5806e795f194ca844cadfe
Tanzim <ahmmadnoorytanzim@gmail.com>
2025-11-20 14:23:38 +0600

    updated

--- main/backend/fileserver/src/main/java/com/example/fileserver/service/FileService.java
+++
@@ -1,8 +0,0 @@
-package com.example.fileserver.service;
-
-import org.springframework.core.io.buffer.DataBuffer;
-import reactor.core.publisher.Flux;
-
-public interface FileService {
-    Flux<DataBuffer> getFileAsStream(String filename);
-}
